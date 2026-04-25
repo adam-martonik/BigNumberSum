3f0735ea8616db63dc70bac8fed975c3c8d00471
Adam Martonik <adam.martonik@student.tuke.sk>
2026-04-25 11:32:22 +0200

    implemented few basic tests for NumberGenerator and BigNumberAdder

--- Tests/BigNumberAdderTests.cs
+++ Tests/BigNumberAdderTests.cs
@@ -1,6 +1,27 @@
-﻿namespace DefaultNamespace;
+﻿using Core;
+using Xunit;
+
+namespace Tests;
 
 public class BigNumberAdderTests
 {
-    
+    private BigNumberAdder adder = new BigNumberAdder();
+
+    [Theory]
+    [InlineData("1", "1", "2")]
+    [InlineData("11111111111111111111111111111111111111111111111111",
+        "22222222222222222222222222222222222222222222222222",
+        "33333333333333333333333333333333333333333333333333")]
+    [InlineData("99999", "1", "100000")]
+    [InlineData("99999999999999999999999999999999999999999999999999",
+        "99999999999999999999999999999999999999999999999999",
+        "199999999999999999999999999999999999999999999999998")]
+    [InlineData("0", "123", "123")]
+    [InlineData("123", "0", "123")]
+    [InlineData("9999999999", "1", "10000000000")]
+    [InlineData("9", "9", "18")]
+    public void Add(string a, string b, string expected)
+    {
+        Assert.Equal(expected, adder.Add(a, b));
+    }
 }
--- Tests/NumberGeneratorTests.cs
+++ Tests/NumberGeneratorTests.cs
@@ -1,6 +1,19 @@
-﻿namespace DefaultNamespace;
-
+﻿namespace Tests;
+using Core;
+using Xunit;
 public class NumberGeneratorTests
 {
+    private NumberGenerator generator = new NumberGenerator(); 
+    [Theory]
+    [InlineData(50)]
+    [InlineData(1)]
+    [InlineData(34)]
+    [InlineData(0)]
+    [InlineData(49)]
+    [InlineData(15)]
     
+    public void nthLengthNumber(int len)
+    {
+        Assert.Equal(len, generator.GenerateNumber(len).Length);
+    }
 }
