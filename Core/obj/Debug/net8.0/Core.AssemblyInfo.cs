0bf6070db132c9757133ac0bbe77fafa79d7960a
Adam Martonik <adam.martonik@student.tuke.sk>
2026-05-25 17:31:32 +0200

    csproj files updated

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1969e319a095e3fcb4f0d6171440c9e50adb5dc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab3e5c4f8e1d68c6eed3037f863ade750c7e4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
